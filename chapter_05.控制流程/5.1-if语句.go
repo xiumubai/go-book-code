0803783e07ae23374f19f3c29a7db355eee32d5d
xiumubai <1547702880@qq.com>
2022-08-29 14:57:27 +0800

    feat: 5.1-if语句

---
+++ chapter_05.控制流程/5.1-if语句.go
@@ -0,0 +1,23 @@
+/*
+ * @Author: 朽木白
+ * @Date: 2022-08-29 14:06:26
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-08-29 14:56:04
+ * @Description: 第一个go程序
+ */
+ package main
+
+ import (
+	 "fmt"
+ )
+ 
+ func main() {
+	b := 3
+	if b == 1 {
+		fmt.Println(1)
+	} else if b == 2 {
+		fmt.Println(2)
+	} else {
+		fmt.Println(3)
+	}
+ }
