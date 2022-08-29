78a268f5e4d7a08463a4484701df9570e9a75a96
xiumubai <1547702880@qq.com>
2022-08-29 16:56:35 +0800

    feat: 8.4-接口

---
+++ chapter_08.方法和接口/8.4-接口.go
@@ -0,0 +1,68 @@
+/*
+ * @Author: 朽木白
+ * @Date: 2022-08-29 14:06:26
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-08-29 16:54:34
+ * @Description: 第一个go程序
+ */
+package main
+import (
+	"errors"
+	"fmt"
+)
+
+type Robot interface {
+	PowerOn() error
+}
+
+type T850 struct {
+	Name string
+}
+
+func (a *T850) PowerOn() error {
+	return nil
+}
+
+type R2D2 struct {
+	Broken bool
+}
+
+func (r *R2D2) PowerOn() error {
+	if r.Broken {
+		return errors.New("R2D2 is broken")
+	} else {
+		return nil
+	}
+}
+
+func Boot(r Robot) error {
+	return r.PowerOn()
+}
+
+func main() {
+
+	t := T850{
+		Name: "The Terminator",
+	}
+
+	r := R2D2{
+		Broken: true,
+	}
+
+	err := Boot(&r)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Robot is powered on!")
+	}
+
+	err = Boot(&t)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Robot is powered on!")
+	}
+
+}
